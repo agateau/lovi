7e55c3eb8d3f08fbc8579fdf102600818ba260fd
Heimen Stoffels <vistausss@fastmail.com>
2023-04-21 14:38:55 +0200

    Added Dutch translation

---
+++ src/translations/lovi_nl.ts
@@ -0,0 +1,309 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>ColorWidget</name>
+    <message>
+        <location filename="src/ui/ColorWidget.cpp" line="113"/>
+        <source>None</source>
+        <translation>Geen</translation>
+    </message>
+    <message>
+        <location filename="src/ui/ColorWidget.cpp" line="114"/>
+        <source>Auto</source>
+        <translation>Automatisch</translation>
+    </message>
+</context>
+<context>
+    <name>ConditionIO</name>
+    <message>
+        <location filename="src/core/ConditionIO.cpp" line="87"/>
+        <source>Failed to parse &apos;%1&apos;</source>
+        <translation>‘%1’ kan niet worden verwerkt</translation>
+    </message>
+    <message>
+        <location filename="src/core/ConditionIO.cpp" line="92"/>
+        <source>Expected 3 tokens in &apos;%1&apos;</source>
+        <translation>Er werden 3 toegangssleutels verwacht in ‘%1’</translation>
+    </message>
+    <message>
+        <location filename="src/core/ConditionIO.cpp" line="101"/>
+        <source>No column named &apos;%1&apos;</source>
+        <translation>Er is geen kolom genaamd “%1”</translation>
+    </message>
+    <message>
+        <location filename="src/core/ConditionIO.cpp" line="112"/>
+        <source>&apos;%1&apos; is not a valid regex: %2</source>
+        <translation>‘%1’ is geen geldige uitdrukking: %2</translation>
+    </message>
+    <message>
+        <location filename="src/core/ConditionIO.cpp" line="117"/>
+        <source>Invalid operator &apos;%1&apos;</source>
+        <translation>Ongeldige operator: ‘%1’</translation>
+    </message>
+</context>
+<context>
+    <name>ConditionLineEdit</name>
+    <message>
+        <location filename="src/ui/ConditionLineEdit.cpp" line="24"/>
+        <source>&lt;html&gt;The syntax for a condition is:
+
+&lt;pre&gt;[COLUMN] [OPERATOR] [CRITERIA]&lt;/pre&gt;
+
+&lt;p&gt;&lt;b&gt;COLUMN&lt;/b&gt;: The name of a column defined in the log parser.&lt;/p&gt;&lt;p&gt;&lt;b&gt;OPERATOR&lt;/b&gt;: One of:&lt;ul&gt;  &lt;li&gt;= or ==: exact match&lt;/li&gt;  &lt;li&gt;contains: the cell must contain the &quot;criteria&quot; string&lt;/li&gt;  &lt;li&gt;~: the cell must match the &quot;criteria&quot; regular expression&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;p&gt;&lt;b&gt;CRITERIA&lt;/b&gt;: The string to match. If it contains spaces, surround it with &lt;b&gt;&quot;&lt;/b&gt;. To use a literal &lt;b&gt;&quot;&lt;/b&gt;, escape it by prefixing it with another &lt;b&gt;&quot;&lt;/b&gt;.&lt;/p&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;De syntaxis voor een omstandigheid is:
+
+&lt;pre&gt;[KOLOM] [OPERATOR] [CRITERIA]&lt;/pre&gt;
+
+&lt;p&gt;&lt;b&gt;KOLOM&lt;/b&gt;: de naam van een kolom, zoals opgegeven in de logboekverwerking.&lt;/p&gt;&lt;p&gt;&lt;b&gt;OPERATOR&lt;/b&gt;: een van de volgende:&lt;ul&gt;  &lt;li&gt;= of ==: exacte overeenkomst&lt;/li&gt;  &lt;li&gt;bevat: de cel dient een van de ‘criteria’ te bevatten&lt;/li&gt;  &lt;li&gt;~: de cel dient de reguliere uitdrukking van de ‘criteria’ te bevatten&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;p&gt;&lt;b&gt;CRITERIA&lt;/b&gt;: de tekenreeks die overeen dient te komen. Als deze spaties bevat, zet er dan &lt;b&gt;&quot;&lt;/b&gt;omheen. Een daadwerkelijke &lt;b&gt;&quot;&lt;/b&gt; kan worden gebruikt door deze te voorzien van een tweede &lt;b&gt;&quot;&lt;/b&gt;.&lt;/p&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>CustomColorItem</name>
+    <message>
+        <location filename="src/ui/ColorWidget.cpp" line="97"/>
+        <source>Custom...</source>
+        <translation>Aangepast…</translation>
+    </message>
+</context>
+<context>
+    <name>FilterModel</name>
+    <message>
+        <location filename="src/core/FilterModel.cpp" line="61"/>
+        <source>&lt;empty&gt;</source>
+        <translation>&lt;leeg&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>HighlightModel</name>
+    <message>
+        <location filename="src/core/HighlightModel.cpp" line="63"/>
+        <source>&lt;empty&gt;</source>
+        <translation>&lt;leeg&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>HighlightWidget</name>
+    <message>
+        <location filename="src/ui/HighlightWidget.ui" line="14"/>
+        <source>Form</source>
+        <translation>Formulier</translation>
+    </message>
+    <message>
+        <location filename="src/ui/HighlightWidget.ui" line="42"/>
+        <source>Condition:</source>
+        <translation>Omstandigheid:</translation>
+    </message>
+    <message>
+        <location filename="src/ui/HighlightWidget.ui" line="53"/>
+        <source>Cell</source>
+        <translation>Cel</translation>
+    </message>
+    <message>
+        <location filename="src/ui/HighlightWidget.ui" line="58"/>
+        <source>Row</source>
+        <translation>Rij</translation>
+    </message>
+</context>
+<context>
+    <name>LogFormatStore</name>
+    <message>
+        <location filename="src/core/LogFormatStore.cpp" line="74"/>
+        <source>There is already a log format named &apos;%1&apos;.</source>
+        <translation>Er is al een logboekformaat genaamd ‘%1’.</translation>
+    </message>
+    <message>
+        <location filename="src/core/LogFormatStore.cpp" line="78"/>
+        <source>Could not create directory &apos;%1&apos; to store log format.</source>
+        <translation>De map ‘%1’ kan niet worden aangemaakt.</translation>
+    </message>
+    <message>
+        <location filename="src/core/LogFormatStore.cpp" line="85"/>
+        <source>Could not create file &apos;%1&apos;.</source>
+        <translation>Het bestand ‘%1’ kan niet worden aangemaakt.</translation>
+    </message>
+</context>
+<context>
+    <name>LogFormatWidget</name>
+    <message>
+        <location filename="src/ui/LogFormatWidget.ui" line="14"/>
+        <source>Format</source>
+        <translation>Formaat</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.ui" line="26"/>
+        <source>Log format:</source>
+        <translation>Logboekformaat:</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.ui" line="46"/>
+        <source>+</source>
+        <translation>+</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.ui" line="66"/>
+        <source>Parser:</source>
+        <translation>Verwerker:</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.ui" line="86"/>
+        <source>Highlights</source>
+        <translation>Markeringen</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.ui" line="134"/>
+        <source>&amp;Previous</source>
+        <translation>&amp;Vorige</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.ui" line="141"/>
+        <source>&amp;Next</source>
+        <translation>Volge&amp;nde</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.ui" line="165"/>
+        <source>Filters</source>
+        <translation>Filters</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.ui" line="181"/>
+        <source>Hide lines matching any of</source>
+        <translation>Regels verbergen die het volgende bevatten</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.ui" line="186"/>
+        <source>Show lines matching any of</source>
+        <translation>Regels tonen die het volgende bevatten</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.ui" line="197"/>
+        <source>Condition:</source>
+        <translation>Omstandigheid:</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.cpp" line="41"/>
+        <source>&lt;qt&gt;&lt;p&gt;The parser is a regular expression with named groups. For example, this line:&lt;/p&gt;&lt;pre&gt;12:34:56 myapp:1234 debug Start app&lt;/pre&gt;&lt;p&gt;can be parsed with:&lt;/p&gt;&lt;pre&gt;(?&amp;lt;time&gt;\d+:\d+:\d+) (?&amp;lt;app&gt;\w+):(?&amp;lt;pid&gt;\d+) (?&amp;lt;level&gt;\w+) (?&amp;lt;message&gt;.*)&lt;/pre&gt;&lt;/qt&gt;</source>
+        <translation>&lt;qt&gt;&lt;p&gt;De verwerker is een reguliere uitdrukking met groepsnamen. Voorbeeld:&lt;/p&gt;&lt;pre&gt;12:34:56 myapp:1234 debug Start app&lt;/pre&gt;&lt;p&gt;kan worden verwerkt met behulp van:&lt;/p&gt;&lt;pre&gt;(?&amp;lt;time&gt;\d+:\d+:\d+) (?&amp;lt;app&gt;\w+):(?&amp;lt;pid&gt;\d+) (?&amp;lt;level&gt;\w+) (?&amp;lt;message&gt;.*)&lt;/pre&gt;&lt;/qt&gt;</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.cpp" line="126"/>
+        <source>Remove Highlight</source>
+        <translation>Markering wissen</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.cpp" line="168"/>
+        <source>Remove Filter</source>
+        <translation>Filter wissen</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.cpp" line="243"/>
+        <source>Log format name</source>
+        <translation>Logboekformaatnaam</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.cpp" line="243"/>
+        <source>Enter a name for the new log format</source>
+        <translation>Geef het nieuwe logboekformaat een naam</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.cpp" line="252"/>
+        <source>Could not add format.</source>
+        <translation>Het formaat kan niet worden toegevoegd.</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.cpp" line="298"/>
+        <source>No match found</source>
+        <translation>Er zijn geen overeenkomsten</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.cpp" line="301"/>
+        <source>Hit top, continuing at bottom</source>
+        <translation>De bovenkant is bereikt - er wordt vanaf de onderkant gezocht…</translation>
+    </message>
+    <message>
+        <location filename="src/ui/LogFormatWidget.cpp" line="304"/>
+        <source>Hit bottom, continuing at top</source>
+        <translation>De onderkant is bereikt - er wordt vanaf de bovenkant gezocht…</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="src/ui/MainWindow.ui" line="14"/>
+        <source>Lovi</source>
+        <translation>Lovi</translation>
+    </message>
+    <message>
+        <location filename="src/ui/MainWindow.ui" line="54"/>
+        <source>toolBar</source>
+        <translation>Werkbalk</translation>
+    </message>
+    <message>
+        <location filename="src/ui/MainWindow.ui" line="83"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Openen</translation>
+    </message>
+    <message>
+        <location filename="src/ui/MainWindow.ui" line="86"/>
+        <location filename="src/ui/MainWindow.cpp" line="175"/>
+        <source>Open log file</source>
+        <translation>Open een logboek</translation>
+    </message>
+    <message>
+        <location filename="src/ui/MainWindow.ui" line="98"/>
+        <source>Auto scroll</source>
+        <translation>Automatisch schuiven</translation>
+    </message>
+    <message>
+        <location filename="src/ui/MainWindow.ui" line="101"/>
+        <source>Automatically scroll down when new lines are logged</source>
+        <translation>Verschuif de inhoud automatisch zodra er nieuwe regels verschijnen</translation>
+    </message>
+    <message>
+        <location filename="src/ui/MainWindow.cpp" line="136"/>
+        <source>Copy</source>
+        <translation>Kopiëren</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="src/ui/MainWindow.cpp" line="153"/>
+        <source>Copy %n line(s)</source>
+        <translation>
+            <numerusform>%n regel kopiëren</numerusform>
+            <numerusform>%n regels kopiëren</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="src/ui/MainWindow.cpp" line="174"/>
+        <source>Log files (*.log *.log.* *.txt)</source>
+        <translation>Logboeken (*.log *.log.* *.txt)</translation>
+    </message>
+    <message>
+        <location filename="src/ui/MainWindow.cpp" line="174"/>
+        <source>All files (*)</source>
+        <translation>Alle bestanden (*)</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="src/ui/main.cpp" line="41"/>
+        <source>Log viewer</source>
+        <translation>Logboekweergave</translation>
+    </message>
+    <message>
+        <location filename="src/ui/main.cpp" line="44"/>
+        <source>Log file.</source>
+        <translation>Logboek.</translation>
+    </message>
+    <message>
+        <location filename="src/ui/main.cpp" line="47"/>
+        <source>Log format definition.</source>
+        <translation>Logboekformaatbeschrijving.</translation>
+    </message>
+    <message>
+        <location filename="src/ui/main.cpp" line="48"/>
+        <source>log_format_name</source>
+        <translation>logboekformaatnaam</translation>
+    </message>
+</context>
+</TS>
